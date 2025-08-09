70069ebecc8dd070ff80b53277163068343a14c9
gjnzero91 <gjnzero.studio@gmail.com>
2025-08-09 13:32:04 +0700

    Update

--- js/modules/domHelpers.js
+++
@@ -1,8 +0,0 @@
-// js/modules/indexEvents.js
-// Các hàm thao tác DOM cơ bản.
-
-export const getElement = (id) => document.getElementById(id);
-
-export const redirectTo = (page) => {
-  window.location.href = page;
-};
--- js/modules/indexEvents.js
+++
@@ -1,8 +0,0 @@
-// js/modules/indexEvents.js
-// Sự kiện trang index.
-
-import { getElement, redirectTo } from "./domHelpers.js";
-
-export const setupIndexEventListeners = () => {
-  getElement("getStartedBtn")?.addEventListener("click", () => redirectTo("login.html"));
-};
